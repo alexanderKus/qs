a5cba855f4a9f52edf81d5e77eadcbb1455ac127
Aleksander Kuś <79520681+alexanderKus@users.noreply.github.com>
2023-10-25 21:37:51 +0200

    Prepare Parser

--- qsc/ParserSpace/Parser.cs
+++ qsc/ParserSpace/Parser.cs
@@ -1,57 +1,14 @@
-﻿namespace qsc
+﻿using qsc.LexerSpace;
+
+namespace qsc
 {
     internal sealed class Parser
     {
-        private static int _lookahead;
-
-        public Parser()
+        public Parser(Lexer l)
         {
-            _lookahead = Console.Read();
+            Lex = l;
         }
 
-        public void Expr()
-        {
-            Term();
-            while (true)
-            {
-                if (_lookahead == '+')
-                {
-                    Match('+');
-                    Term();
-                    Console.Write('+');
-                }
-                else if (_lookahead == '-')
-                {
-                    Match('-');
-                    Term();
-                    Console.Write('-');
-                }
-                else
-                {
-                    return;
-                }
-            }
-        }
-
-        private void Term()
-        {
-            if (char.IsDigit((char)_lookahead))
-            {
-                Console.Write((char)_lookahead);
-                Match(_lookahead);
-            }
-            else
-            {
-                throw new Exception("Syntax error!");
-            }
-        }
-
-        private void Match(int t)
-        {
-            if (_lookahead == t)
-                _lookahead = Console.Read();
-            else
-                throw new Exception("Syntax error!");
-        }
+        public Lexer Lex { get; }
     }
 }
